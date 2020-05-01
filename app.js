c4e3f706945d1d8d2344d7ca397421991a72b53e
Baptiste Bidaux <baptistelise@orange.fr>
2020-05-01 19:25:29 +0200

    Premier commit du code

---
+++ app.js
@@ -0,0 +1,70 @@
+const image1 = document.getElementsByClassName('animatedImage1');
+new simpleParallax(image1, {
+    scale: 1.4,
+    delay: 1.5,
+    orientation: "left"
+})
+
+const image2 = document.getElementsByClassName('animatedImage2');
+new simpleParallax(image2, {
+    scale: 1.8,
+    delay: 1.5,
+    orientation: "down"
+})
+
+const image3 = document.getElementsByClassName('animatedImage3');
+new simpleParallax(image3, {
+    scale: 1.4,
+    delay: 1.5,
+    orientation: "up right"
+})
+
+const image4 = document.getElementsByClassName('animatedImage4');
+new simpleParallax(image4, {
+    scale: 1.5,
+    delay: 1.5,
+    orientation: "up",
+})
+
+const image5 = document.getElementsByClassName('animatedImage5');
+new simpleParallax(image5, {
+    scale: 1.4,
+    delay: 1.5,
+    orientation: "right"
+})
+
+const image6 = document.getElementsByClassName('animatedImage6');
+new simpleParallax(image6, {
+    scale: 1.5,
+    delay: 1.5,
+    orientation: "down",
+})
+
+const image7 = document.getElementsByClassName('animatedImage7');
+new simpleParallax(image7, {
+    scale: 1.4,
+    delay: 1.5,
+    orientation: "down left"
+})
+
+const image8 = document.getElementsByClassName('animatedImage8');
+new simpleParallax(image8, {
+    scale: 1.2,
+    delay: 1.5,
+    orientation: "up",
+})
+
+const image9 = document.getElementsByClassName('animatedImage9');
+new simpleParallax(image9, {
+    scale: 1.4,
+    delay: 1.5,
+    orientation: "right"
+})
+
+const image10 = document.getElementsByClassName('animatedImage10');
+new simpleParallax(image10, {
+    scale: 1.5,
+    delay: 1.5,
+    orientation: "up",
+    overflow: true
+})
